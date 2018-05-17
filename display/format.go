32cf1ecef1a8c029a56ebf6d677f3a51a7bb4df9
Marc CB <marc.cbrooks@gmail.com>
2018-05-17 15:56:40 -0400

    add formatting to product id string for display and add color calculation to FmtDelta

--- display/format.go
+++ display/format.go
@@ -4,7 +4,14 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+
+	"github.com/gdamore/tcell"
 )
+
+// FmtPair formats product id to represent currency pair (i.e., "BTC/USD")
+func FmtPair(id string) string {
+	return strings.Join(strings.Split(id, "-"), "/")
+}
 
 // FmtPrice formats price data for display
 // If price is >= 10 it uses 2 decimal places
@@ -17,30 +24,33 @@ func FmtPrice(price string) string {
 	if num >= 10 {
 		num = float64(int64(num*100+0.5)) / 100
 		return fmt.Sprintf("%.2f", num)
-
 	}
 	num = float64(int64(num*100000+0.5)) / 100000
 	return fmt.Sprintf("%.5f", num)
 }
 
 // FmtDelta calculates price delta and provides appropriate formatting
-func FmtDelta(price string, open string) string {
+func FmtDelta(price string, open string) (string, tcell.Color) {
 	if len(price) > 0 && len(open) > 0 {
 		p, err := strconv.ParseFloat(price, 64)
 		if err != nil {
-			return "no data"
+			return "no data", tcell.ColorWhite
 		}
 		o, err := strconv.ParseFloat(open, 64)
 		if err != nil {
-			return "no data"
+			return "no data", tcell.ColorWhite
+
 		}
 		d := (p - o) / o * 100
 		b := strings.Builder{}
 		b.WriteString(strconv.FormatFloat(d, 'f', 2, 64))
 		b.WriteString("%")
-		return b.String()
+		if d >= 0 {
+			return b.String(), tcell.ColorGreen
+		}
+		return b.String(), tcell.ColorRed
 	}
-	return "no data"
+	return "no data", tcell.ColorWhite
 }
 
 // FmtSize formats trade size data with 8 decimal places
