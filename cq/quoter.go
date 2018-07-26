a2077f39c1874bba9eaf7ec706fb4324f5c0b9f4
Marc CB <marc.cbrooks@gmail.com>
2018-07-25 20:39:58 -0400

    housekeeping

--- cq/quoter.go
+++ cq/quoter.go
@@ -10,7 +10,10 @@ type Quoter interface {
 	MarketID() string
 	PairID() string
 
+	// UpdRow updates exchange table row with new quote data and changes
+	// font to bold to signal change in data
 	UpdRow(*tview.Table)
+	// ClrBold changes font of table row back to normal
 	ClrBold(*tview.Table)
 
 	UpdOverviewRow(*tview.Table)
