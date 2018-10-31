83d511959f5b65ad68cd860044dd20d583dc7aa3
Marc CB <marc.cbrooks@gmail.com>
2018-10-31 13:21:46 -0400

    add support for hitBTC exchange

---
+++ hitbtc/hitbtc.go
@@ -0,0 +1,121 @@
+package hitbtc
+
+import (
+	"strings"
+	"sync"
+
+	"github.com/3cb/cq/cq"
+	"github.com/3cb/tview"
+	"github.com/gdamore/tcell"
+)
+
+// Market contains state data
+type Market struct {
+	sync.RWMutex
+	streaming bool
+	symbols   []string
+	data      map[string]cq.Quoter
+}
+
+// Init creates instance of a bitfinex market without quotes
+func Init() *Market {
+	m := &Market{
+		streaming: false,
+		symbols: []string{
+			"BTCUSD",
+			"BCHUSD",
+			"ETHUSD",
+			"ETHBTC",
+			"LTCUSD",
+			"LTCBTC",
+			"ZECUSD",
+			"ZECBTC",
+			"ZRXUSD",
+			"ZRXBTC",
+		},
+		data: make(map[string]cq.Quoter),
+	}
+
+	for _, s := range m.symbols {
+		m.data[s] = Quote{
+			Symbol: s,
+			ID:     formatID(s),
+		}
+	}
+
+	return m
+}
+
+// formats pair with uppercase letters separated by "/"
+func formatID(s string) string {
+	temp := strings.Split(s, "")
+	b := strings.Builder{}
+
+	for i := 0; i < 3; i++ {
+		b.WriteString(temp[i])
+	}
+	b.WriteString("-")
+	for i := 3; i < len(temp); i++ {
+		b.WriteString(temp[i])
+	}
+
+	return b.String()
+}
+
+// Table returns an instance of tview.Table formatted for hitbtc ready for data
+func (m *Market) Table(overviewTbl *tview.Table) *tview.Table {
+	headers := []string{
+		"Pair",
+		"Price",
+		"Change",
+		"Last Size",
+		"Bid",
+		"Ask",
+		"Low",
+		"High",
+		"Volume",
+	}
+
+	tbl := tview.NewTable().
+		SetBorders(false)
+
+	for i, header := range headers {
+		tbl.SetCell(0, i, tview.NewTableCell(header).
+			SetTextColor(tcell.ColorYellow).
+			SetAlign(tview.AlignRight))
+	}
+
+	for r := 1; r <= 25; r++ {
+		for c := 0; c <= 8; c++ {
+			tbl.SetCell(r, c, tview.NewTableCell("").
+				SetAlign(tview.AlignRight))
+		}
+	}
+
+	// handle errors here ***************
+	m.getSnapshot()
+
+	m.Lock()
+	data := m.data
+	m.Unlock()
+
+	for _, quote := range data {
+		quote.UpdRow(tbl)()
+		quote.ClrBold(tbl)()
+		quote.UpdOverviewRow(overviewTbl)()
+		quote.ClrOverviewBold(overviewTbl)()
+	}
+
+	return tbl
+}
+
+// Stream connects to websocket server and streams price quotes
+func (m *Market) Stream(data chan cq.Quoter) error {
+	// var e error
+
+	// if err := connectWS(m, data); err != nil {
+	// 	return err
+	// }
+
+	return nil
+}
---
+++ hitbtc/rest.go
@@ -0,0 +1,149 @@
+package hitbtc
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+// TickerEntry holds data for element of ticker response array
+// https://api.hitbtc.com/api/2/public/ticker
+type TickerEntry struct {
+	Symbol      string `json:"symbol"`
+	Ask         string `json:"ask"`
+	Bid         string `json:"bid"`
+	Last        string `json:"last"`
+	Low         string `json:"low"`
+	High        string `json:"high"`
+	Open        string `json:"open"`
+	Volume      string `json:"volume"`
+	VolumeQuote string `json:"volumeQuote"`
+	Timestamp   string `json:"timestamp"`
+}
+
+// TradesEntry holds data for element of trades response array
+// https://api.hitbtc.com/api/2/public/trades/{symbol}
+type TradesEntry struct {
+	ID        int    `json:"id"`
+	Price     string `json:"price"`
+	Quantity  string `json:"quantity"`
+	Side      string `json:"side"`
+	Timestamp string `json:"timestamp"`
+}
+
+// get initial data for overview and hitbtc tables
+func (m *Market) getSnapshot() []error {
+	var e []error
+
+	m.RLock()
+	symbols := m.symbols
+	m.RUnlock()
+	l := len(symbols)
+	errCh := make(chan error, l+1)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(l + 1)
+	go getTicker(m, wg, errCh)
+	for _, s := range symbols {
+		go getTrades(m, s, wg, errCh)
+	}
+	wg.Wait()
+
+	for _, d := range m.data {
+		m.Lock()
+		fmt.Printf("%v", d)
+		m.Unlock()
+	}
+
+	close(errCh)
+	for err := range errCh {
+		e = append(e, err)
+	}
+
+	return e
+}
+
+// get most recent ticker values for all symbols:
+// https://api.hitbtc.com/api/2/public/ticker
+func getTicker(m *Market, wg *sync.WaitGroup, errCh chan<- error) {
+	defer wg.Done()
+	var data []TickerEntry
+
+	api := "https://api.hitbtc.com/api/2/public/ticker"
+
+	resp, err := http.Get(api)
+	if err != nil {
+		errCh <- err
+		return
+	}
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		errCh <- err
+		return
+	}
+
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		errCh <- err
+		return
+	}
+
+	for _, s := range m.symbols {
+		for _, d := range data {
+			if s == d.Symbol {
+				m.Lock()
+				q := (m.data[s]).(Quote)
+				q.Symbol = s
+				q.Ask = d.Ask
+				q.Bid = d.Bid
+				q.Low = d.Low
+				q.High = d.High
+				q.Open = d.Open
+				q.Volume = d.Volume
+				m.data[s] = q
+				m.Unlock()
+			}
+		}
+	}
+}
+
+// get last trade price and volume data by symbol:
+// https://api.hitbtc.com/api/2/public/trades/{symbol}
+func getTrades(m *Market, symbol string, wg *sync.WaitGroup, errCh chan<- error) {
+	defer wg.Done()
+	var data []TradesEntry
+
+	// get only most recent trade
+	api := "https://api.hitbtc.com/api/2/public/trades/" + symbol + "?limit=1"
+
+	resp, err := http.Get(api)
+	if err != nil {
+		errCh <- err
+		return
+	}
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		errCh <- err
+		return
+	}
+
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		errCh <- err
+		return
+	}
+
+	m.Lock()
+	q := (m.data[symbol]).(Quote)
+	q.Price = data[0].Price
+	q.Size = data[0].Quantity
+	m.data[symbol] = q
+	m.Unlock()
+
+}
